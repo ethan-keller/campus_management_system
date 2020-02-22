0cb7bda92aad1672b1c4e0a0d561c08f6be5995e
Hao Wang <H.Wang-26@student.tudelft.nl>
2020-02-22 13:29:24 +0100

    Login & register view finished, switch scene link functioned.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginViewController.java
@@ -1,27 +1,43 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
+import javafx.event.ActionEvent;
+import nl.tudelft.oopp.demo.views.RegisterView;
+import java.io.IOException;
 
 public class LoginViewController {
 
-    private TextField usernameField;
+    @FXML
+    private TextField username;
 
-    private PasswordField passwordField;
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Hyperlink goToRegister;
 
     /**
-     * Handles clicking the button.
+     * Handles clicking the login button.
      */
-    public void loginButtonClicked() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Quote for you");
-        alert.setHeaderText(null);
-        alert.setContentText(ServerCommunication.getQuote());
-        alert.showAndWait();
+//    public void loginButtonClicked() {
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//        alert.setTitle("Login Status");
+//        alert.setHeaderText(null);
+//        String usernameTxt = username.getText();
+//        String passwordTxt = password.getText();
+//        alert.setContentText(ServerCommunication.sendLogin(usernameTxt, passwordTxt));
+//        alert.showAndWait();
+//    }
+
+    /**
+     * Handles clicking the register link.
+     */
+    public void goToRegisterClicked(ActionEvent event) throws IOException {
+        RegisterView rv = new RegisterView();
+        Stage stage = new Stage();
+        rv.start(stage);
     }
-
-
 }
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
@@ -1,15 +1,45 @@
 package nl.tudelft.oopp.demo.controllers;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 
 public class RegisterViewController {
-    public void registerClicked(ActionEvent actionEvent) {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private PasswordField rePassword;
+
+    @FXML
+    private Label pwd_error;
+
+    /**
+     * Handles clicking the button.
+     */
+    public void registerClicked() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Quote for you");
+        alert.setTitle("Login Status");
         alert.setHeaderText(null);
-        alert.setContentText(ServerCommunication.getQuote());
-        alert.showAndWait();
+
+        String usernameTxt = username.getText();
+        String passwordTxt = password.getText();
+        String rePasswordTxt = rePassword.getText();
+
+        if(! passwordTxt.equals(rePasswordTxt)) {
+            pwd_error.textProperty().set("Password does not match, try again");
+        }
+        else{
+            alert.setContentText("Account created！");
+//            alert.setContentText(ServerCommunication.sendRegister(usernameTxt, passwordTxt));
+//            alert.showAndWait();
+        }
     }
 }
