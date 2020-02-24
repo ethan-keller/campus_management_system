08fc676c84f5dc603dda27e32dbfd8222409949f
dtgupta <d.t.gupta@student.tudelft.nl>
2020-02-24 17:26:00 +0100

    The calender to pick date is added for the date pick option.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/SearchViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/SearchViewController.java
@@ -4,23 +4,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import nl.tudelft.oopp.demo.communication.LoginServerCommunication;
 
 public class SearchViewController {
 
     ObservableList<String> buildingChoice = FXCollections.observableArrayList("EEMCS", "Drebbelweg", "Aula", "Library", "Fellowship");
-    ObservableList<String> dateChoice = FXCollections.observableArrayList("Tomorrow", "Day After");
+//    ObservableList<String> dateChoice = FXCollections.observableArrayList("Today", "Tomorrow", "Day After");
     ObservableList<String> timeChoice = FXCollections.observableArrayList("9:00 - 12:00", "13:00 - 17:00", "17:00 - 20:00");
     ObservableList<String> capacityChoice = FXCollections.observableArrayList("4 people", "6 people", "8 people", "10 people");
     ObservableList<String> roleChoice = FXCollections.observableArrayList("Lecturer Only", "All");
     ObservableList<String> foodChoice = FXCollections.observableArrayList("Cafeteria", "CoffeeStar");
 
     @FXML
-    private ChoiceBox DateChoiceBox;
+    private DatePicker DateChoiceBox;
 
     @FXML
     private ChoiceBox TimeslotChoiceBox;
@@ -50,7 +47,7 @@ public class SearchViewController {
 
     @FXML
     private void initialize() {
-        DateChoiceBox.setItems(dateChoice);
+//        DateChoiceBox.setItems(dateChoice);
         TimeslotChoiceBox.setItems(timeChoice);
         BuildingChoiceBox.setItems(buildingChoice);
         CapacityChoiceBox.setItems(capacityChoice);
