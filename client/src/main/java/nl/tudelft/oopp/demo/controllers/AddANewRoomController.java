e5a2a7af699136820fab106e5bb03b621d54c34f
Ryo Iizuka <r.iizuka@student.tudelft.nl>
2020-03-02 14:18:08 +0100

    AddANewRoomController improved

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/AddANewRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/AddANewRoomController.java
@@ -1,9 +1,9 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.lang.reflect.Type;
 
 public class AddANewRoomController {
 
@@ -23,11 +23,51 @@ public class AddANewRoomController {
     private TextField RoomNameTextField;
 
     @FXML
-    private TextField FacilitiesTextField;
+    private TextArea FacilitiesTextField;
 
     @FXML
     private Spinner BikeSpinner;
 
     @FXML
-    private TextField FoodAvailableTextField;
+    private TextArea FoodAvailableTextField;
+
+    @FXML
+    private Button AddButton;
+
+    @FXML
+    private Button AddRoomButton;
+
+    public void AddRoomButtonClicked(){
+        String room = AddRoomButton.getText();
+        BuildingChoiceBox.getItems().add(room);
+    }
+
+    public void AddButtonClicked(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        if(BuildingChoiceBox==null){
+            alert.setTitle("Please select the Building");
+            alert.show();
+        }
+        else if(CapacityTextField == null){
+            alert.setTitle("Please enter the capacity");
+            alert.show();
+        }
+        else if(TypeChoiceBox == null){
+            alert.setTitle("Please select the Type");
+            alert.show();
+        }
+        else if(BikeSpinner==null){
+            alert.setTitle("Please select the number of bikes available");
+            alert.show();
+        }
+        else if(RoomNameTextField==null){
+            alert.setTitle("Please provide the name of the building");
+            alert.show();
+        }
+        else{
+            //Room room = new Ro
+        }
+    }
+
+
 }
