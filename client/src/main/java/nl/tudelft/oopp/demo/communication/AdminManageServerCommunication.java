96bc8c8f90eb31d4de4d26e1c9d7a46a0938ca3a
Mathijs <m.c.l.vanderschoot@student.tudelft.nl>
2020-03-02 15:38:45 +0100

    Added encoding to admin controller

--- client/src/main/java/nl/tudelft/oopp/demo/communication/AdminManageServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/AdminManageServerCommunication.java
@@ -1,17 +1,22 @@
 package nl.tudelft.oopp.demo.communication;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 public class AdminManageServerCommunication {
 
     private static HttpClient client = HttpClient.newBuilder().build();
 
     public static void createRoom(String name, int building, boolean teacher_only, int capacity, String photos, String description, String type) {
-        String params = "?name="+name+"&building="+building+"&teacher_only="+teacher_only+"&capacity="+capacity+"&photos="+photos+"&description="+description+"&type="+type;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createRoom"+params)).build();
+        String params = "name="+name+"&building="+building+"&teacher_only="+teacher_only+"&capacity="+capacity+"&photos="+photos+"&description="+description+"&type="+type;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createRoom?"+params)).build();
         HttpResponse<String> response = null;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -24,8 +29,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void updateRoom(int id, String name, int building, boolean teacher_only, int capacity, String photos, String description, String type){
-        String params = "?id="+id+"&name="+name+"&building="+building+"&teacher_only="+teacher_only+"&capacity="+capacity+"&photos="+photos+"&description="+description+"&type="+type;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateRoom"+params)).build();
+        String params = "id="+id+"&name="+name+"&building="+building+"&teacher_only="+teacher_only+"&capacity="+capacity+"&photos="+photos+"&description="+description+"&type="+type;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateRoom?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -38,8 +45,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void deleteRoom(int id){
-        String params = "?id="+id;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteRoom"+params)).build();
+        String params = "id="+id;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteRoom?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -52,8 +61,10 @@ public class AdminManageServerCommunication {
     }
 
     public static String getRoom(int id){
-        String params = "?id="+id;
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getRoom"+params)).build();
+        String params = "id="+id;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getRoom?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -95,8 +106,10 @@ public class AdminManageServerCommunication {
     }
 
     public static String getBuilding(int id){
-        String params = "?id="+id;
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getBuilding"+params)).build();
+        String params = "id="+id;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getBuilding?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -110,8 +123,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void createBuilding(String name, int room_count, String address){
-        String params = "?name="+name+"&room_count="+room_count+"&address="+address;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createBuilding"+params)).build();
+        String params = "name="+name+"&room_count="+room_count+"&address="+address;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createBuilding?"+params)).build();
         HttpResponse<String> response = null;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -124,8 +139,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void deleteBuilding(int id){
-        String params = "?id="+id;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteBuilding"+params)).build();
+        String params = "id="+id;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteBuilding?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -138,8 +155,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void updateBuilding(int id, String name, int room_count, String address){
-        String params = "?id="+id+"&name="+name+"&room_count="+room_count+"&address="+address;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateBuilding"+params)).build();
+        String params = "id="+id+"&name="+name+"&room_count="+room_count+"&address="+address;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateBuilding?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -166,8 +185,10 @@ public class AdminManageServerCommunication {
     }
 
     public static String getUser(String username){
-        String params = "?username="+username;
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getUser"+params)).build();
+        String params = "username="+username;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getUser?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -181,8 +202,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void deleteUser(String username){
-        String params = "?username="+username;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteUser"+params)).build();
+        String params = "username="+username;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteUser?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -195,8 +218,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void updateUser(String username, String password, int type){
-        String params = "?username="+username+"&password="+password+"&type="+type;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateUser"+params)).build();
+        String params = "username="+username+"&password="+password+"&type="+type;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateUser?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -209,8 +234,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void createUser(String username, String password, int type){
-        String params = "?username="+username+"&password="+password+"&type="+type;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createUser"+params)).build();
+        String params = "username="+username+"&password="+password+"&type="+type;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createUser?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -223,8 +250,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void createReservation(String username, int room, String date, String starting_time, String ending_time){
-        String params = "?username="+username+"&room="+room+"&date="+date+"&starting_time="+starting_time+"ending_time="+ending_time;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createReservation"+params)).build();
+        String params = "username="+username+"&room="+room+"&date="+date+"&starting_time="+starting_time+"ending_time="+ending_time;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/createReservation?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -237,8 +266,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void updateReservation(int id, int room, String date, String starting_time, String ending_time){
-        String params = "?id="+id+"&room="+room+"&date="+date+"&starting_time="+starting_time+"ending_time="+ending_time;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateReservation"+params)).build();
+        String params = "id="+id+"&room="+room+"&date="+date+"&starting_time="+starting_time+"ending_time="+ending_time;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/updateReservation?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -251,8 +282,10 @@ public class AdminManageServerCommunication {
     }
 
     public static void deleteReservation(int id){
-        String params = "?id="+id;
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteReservation"+params)).build();
+        String params = "id="+id;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody()).uri(URI.create("http://localhost:8080/deleteReservation?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -265,8 +298,10 @@ public class AdminManageServerCommunication {
     }
 
     public static String getReservation(int id){
-        String params = "?id="+id;
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getReservation"+params)).build();
+        String params = "id="+id;
+        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/getReservation?"+params)).build();
         HttpResponse<String> response = null;
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
