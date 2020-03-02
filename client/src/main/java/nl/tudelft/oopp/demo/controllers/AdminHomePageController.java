cc76dc832485d53ce2a05d75073d1f026439e190
dtgupta <d.t.gupta@student.tudelft.nl>
2020-03-02 14:33:00 +0100

    Minor fixes

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminHomePageController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminHomePageController.java
@@ -5,6 +5,7 @@ import javafx.scene.Node;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import nl.tudelft.oopp.demo.views.AdminHomePageView;
 import nl.tudelft.oopp.demo.views.AdminManageRoomView;
 import nl.tudelft.oopp.demo.views.LoginView;
 
@@ -13,6 +14,8 @@ import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class AdminHomePageController {
+
+    public AdminHomePageView adminHomePageView;
 
     public void signOutButtonClicked(ActionEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
