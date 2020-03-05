364b24938285fed7574463f283d978b3dc2cdbe3
lverhaegh <L.Verhaegh@student.tudelft.nl>
2020-03-05 16:00:26 +0100

    Added JavaDoc stubs to BuildingController (serverside)

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/BuildingController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/BuildingController.java
@@ -16,15 +16,20 @@ import java.nio.charset.StandardCharsets;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-/**
- *
- */
+
 @Controller
 public class BuildingController {
 
     @Autowired
     private BuildingRepository buildingRepo;
 
+    /**
+     * Adds a building to the database.
+     * @param name The name of the building.
+     * @param room_count The amount of of rooms inside the building.
+     * @param address The address of the building. //TODO format of address!!
+     * @throws UnsupportedEncodingException
+     */
     @PostMapping("createBuilding")
     @ResponseBody
     public void createBuilding(@RequestParam String name, @RequestParam int room_count, @RequestParam String address) throws UnsupportedEncodingException {
@@ -37,6 +42,14 @@ public class BuildingController {
         }
     }
 
+    /**
+     * Changes the existing building with the provided ID in the database with the provides parameters
+     * @param id The building ID, this is the building that is going to get changed.
+     * @param name The new name of the building
+     * @param room_count the new room count of the building
+     * @param address the new address of the building //TODO add address format
+     * @throws UnsupportedEncodingException
+     */
     @PostMapping("updateBuilding")
     @ResponseBody
     public void updateBuilding(@RequestParam int id, @RequestParam String name, @RequestParam int room_count, @RequestParam String address) throws UnsupportedEncodingException {
@@ -52,6 +65,10 @@ public class BuildingController {
         }
     }
 
+    /**
+     * Deletes the building from the database.
+     * @param id The ID of the building you delete.
+     */
     @PostMapping("deleteBuilding")
     @ResponseBody
     public void deleteBuilding(@RequestParam int id){
@@ -62,6 +79,11 @@ public class BuildingController {
         }
     }
 
+    /**
+     * Returns the database entry of the building.
+     * @param id The ID of the building you requested info of.
+     * @return //TODO figure out the exact way it's returned.
+     */
     @GetMapping("getBuilding")
     @ResponseBody
     public Building getBuilding(@RequestParam int id){
@@ -73,6 +95,10 @@ public class BuildingController {
         return null;
     }
 
+    /**
+     * Returns all the buildings
+     * @return //TODO figure out the exact way it's returned.
+     */
     @GetMapping("getAllBuildings")
     @ResponseBody
     public List<Building> getAllBuildings(){
