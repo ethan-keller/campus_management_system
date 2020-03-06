e2cc82d67552a793e71e525ea2afb495b176e172
dtgupta <d.t.gupta@student.tudelft.nl>
2020-03-06 20:31:07 +0100

    Added all password field constraints and comments

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
@@ -12,6 +12,7 @@ import nl.tudelft.oopp.demo.communication.RegisterServerCommunication;
 import nl.tudelft.oopp.demo.views.LoginView;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.regex.Pattern;
 
 public class RegisterViewController {
 
@@ -45,28 +46,56 @@ public class RegisterViewController {
         String passwordTxt = password.getText();
         String rePasswordTxt = rePassword.getText();
 
+        Pattern UpperCasePattern = Pattern.compile("[A-Z ]");
+
+        /**
+         *Checks whether the password username field is left empty or not.
+         **/
         if(username.getText().trim().isEmpty()) {
             usernameLabel.setText("This field cannot be left empty !");
             usernameLabel.setStyle("-fx-text-fill: red");
-        }
+        }/**
+         *Checks whether the password field is left empty.
+         **/
         if(password.getText().trim().isEmpty()) {
             passwordLabel.setText("This field cannot be left empty !");
             passwordLabel.setStyle("-fx-text-fill: red");
-        }
+        }/**
+         *Checks whether the password field is atleast 8 characters long.
+         **/
         if(password.getText().length() < 8) {
             passwordLabel.setText("The password needs to have atleast 8 characters !");
             passwordLabel.setStyle("-fx-text-fill: red");
-        }
+        }/**
+         *Checks whether the password matches the repeat password field
+         **/
         if(!passwordTxt.equals(rePasswordTxt)) {
             rePasswordLabel.setText("The password needs to be the same !");
             rePasswordLabel.setStyle("-fx-text-fill: red");
+        }/**
+         *Checks whether the password contains atleast one numeric value
+         **/
+        if(!passwordTxt.matches(".*\\d.*")) {
+            passwordLabel.setText("The password needs to have atleast 1 numeric value !");
+            passwordLabel.setStyle("-fx-text-fill: red");
         }
+        if(!UpperCasePattern.matcher(passwordTxt).find()) {
+            passwordLabel.setText("The password needs to have atleast 1 Upper Case letter !");
+            passwordLabel.setStyle("-fx-text-fill: red");
+        }/**
+         *Server connection is established.
+         **/
         else {
             alert.setContentText(RegisterServerCommunication.sendRegister(usernameTxt, passwordTxt));
             alert.showAndWait();
         }
     }
 
+    /**
+     *On the click of the back button, the stage is redirected to the login page.
+     * @param event
+     * @throws IOException
+     */
     public void backClicked(ActionEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
