ec997e465183f34a80708c077f747a5588d0477e
Ryo Iizuka <r.iizuka@student.tudelft.nl>
2020-03-13 14:03:50 +0100

    Rooms available on SearchView.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RoomViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RoomViewController.java
@@ -1,7 +1,5 @@
 package nl.tudelft.oopp.demo.controllers;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -12,6 +10,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import jdk.jfr.Event;
@@ -49,45 +48,58 @@ public class RoomViewController implements Initializable {
     private Text RoomCapacity;
 
     @FXML
-    private Text RoomFacilities;
-
-    @FXML
     private Text RoomBikes;
 
     @FXML
     private Text RoomAvailability;
 
 
+    public  void setRoomBuilding(String building){
+        RoomBuilding.setText("Building: "+building);
+    }
+    public void setRoomCapacity(int capacity){
+        RoomCapacity.setText("Capacity: "+capacity);
+    }
+    public void setRoomBikes(int bikes){
+        RoomBikes.setText("Bikes available: "+bikes);
+    }
+    public void setRoomAvailability(boolean boo){
+        if(boo){
+            RoomAvailability.setText("Teachers and Students");
+        }
+        else{
+            RoomAvailability.setText("Teachers only");
+        }
+    }
+    public void setImage(Image pic){
+        RoomImage.setImage(pic);
+    }
+
+
+
+
+
+
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        //Adding choices for Timeslot choicebox
-        List<String> TimeslotList = new ArrayList<String>();
-        TimeslotList.add("8:00 - 9:00");
-        TimeslotList.add("9:00 - 10:00");
-        TimeslotList.add("10:00 - 11:00");
-        TimeslotList.add("11:00 - 12:00");
-        TimeslotList.add("12:00 - 13:00");
-        TimeslotList.add("13:00 - 14:00");
-        TimeslotList.add("14:00 - 15:00");
-        TimeslotList.add("15:00 - 16:00");
-        TimeslotList.add("16:00 - 17:00");
-        TimeslotList.add("17:00 - 18:00");
-        TimeslotList.add("18:00 - 19:00");
-        TimeslotList.add("19:00 - 20:00");
-        TimeslotList.add("20:00 - 21:00");
-        TimeslotList.add("21:00 - 22:00");
-        TimeslotList.add("22:00 - 23:00");
-        TimeslotList.add("23:00 - 24:00");
-        ObservableList TimeslotobList = FXCollections.observableList(TimeslotList);
-        TimeslotChoiceBox.setItems(TimeslotobList);
+        ObservableList<String> TimeslotList = FXCollections.observableArrayList();
+        ObservableList<String> FoodList = FXCollections.observableArrayList();
 
 
-        //Adding choices for food choice box
-        List<String> FoodList = new ArrayList<String>();
-        FoodList.add("Add food here from the database");
-        ObservableList FoodobList = FXCollections.observableList(FoodList);
-        FoodChoiceBox.setItems(FoodobList);
+        TimeslotList.addAll("8:00 - 9:00","10:00 - 11:00","11:00 - 12:00",
+                "12:00 - 13:00","13:00 - 14:00","14:00 - 15:00","15:00 - 16:00",
+                "16:00 - 17:00","17:00 - 18:00","18:00 - 19:00","19:00 - 20:00",
+                "20:00 - 21:00","21:00 - 22:00","23:00 - 24:00");
+        FoodList.addAll("Ham Sandwich", "Cheese Sandwich", "Pasta");
+
+        TimeslotChoiceBox.setItems(TimeslotList);
+        FoodChoiceBox.setItems(FoodList);
+
+
 
     }
 
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/SearchViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/SearchViewController.java
@@ -1,30 +1,30 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.views.RoomView;
+
+
+
+
+import java.io.IOException;
 
 public class SearchViewController {
 
 
-    @FXML
-    private DatePicker DateChoiceBox;
-
-    @FXML
-    private ChoiceBox TimeslotChoiceBox;
-
+    public TextField textfield;
     @FXML
     private ChoiceBox BuildingChoiceBox;
 
     @FXML
-    private ChoiceBox CapacityChoiceBox;
+    private ChoiceBox TimeslotChoiceBox;
 
     @FXML
     private ChoiceBox RoleChoiceBox;
@@ -33,113 +33,145 @@ public class SearchViewController {
     private ChoiceBox FoodAvailableChoiceBox;
 
     @FXML
-    private TextField SearchBar;
+    private ChoiceBox CapacityChoiceBox;
 
     @FXML
-    private ImageView ImageToBeAdded;
-
-    @FXML
-    private Text BuildingToBeAdded;
-
-    @FXML
-    private Text RoomToBeAdded;
-
-    @FXML
-    private Text CapacityToBeAdded;
+    private Button CancelBookingButton;
 
     @FXML
     private Button BookingHistoryButton;
 
     @FXML
-    private Button CancelBookingButton;
-
-    public void SearchBarClicked() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Login Status");
-        alert.setHeaderText(null);
-        String usernameTxt = SearchBar.getText();
-        //////THIS REQUIRES SEARCH ALGORITHM
-        // TO BE ADDED WITH DATABASE GROUP
-    }
-
-
-    public void setRoom(Image im, String buildingName, String roomName, String capacityNumber) {
-        ImageToBeAdded.setImage(im);
-        BuildingToBeAdded.setText(buildingName);
-        RoomToBeAdded.setText(roomName);
-        CapacityToBeAdded.setText(capacityNumber);
-
-    }
-
-    public void initializeTimeSlot() {
-        TimeslotChoiceBox.getItems().add("0:00 - 1:00");
-        TimeslotChoiceBox.getItems().add("1:00 - 2:00");
-        TimeslotChoiceBox.getItems().add("2:00 - 3:00");
-        TimeslotChoiceBox.getItems().add("3:00 - 4:00");
-        TimeslotChoiceBox.getItems().add("4:00 - 5:00");
-        TimeslotChoiceBox.getItems().add("5:00 - 6:00");
-        TimeslotChoiceBox.getItems().add("6:00 - 7:00");
-        TimeslotChoiceBox.getItems().add("7:00 - 8:00");
-        TimeslotChoiceBox.getItems().add("8:00 - 9:00");
-        TimeslotChoiceBox.getItems().add("9:00 - 10:00");
-        TimeslotChoiceBox.getItems().add("10:00 - 11:00");
-        TimeslotChoiceBox.getItems().add("11:00 - 12:00");
-        TimeslotChoiceBox.getItems().add("12:00 - 13:00");
-        TimeslotChoiceBox.getItems().add("13:00 - 14:00");
-        TimeslotChoiceBox.getItems().add("14:00 - 15:00");
-        TimeslotChoiceBox.getItems().add("15:00 - 16:00");
-        TimeslotChoiceBox.getItems().add("16:00 - 17:00");
-        TimeslotChoiceBox.getItems().add("17:00 - 18:00");
-        TimeslotChoiceBox.getItems().add("18:00 - 19:00");
-        TimeslotChoiceBox.getItems().add("19:00 - 20:00");
-        TimeslotChoiceBox.getItems().add("20:00 - 21:00");
-        TimeslotChoiceBox.getItems().add("21:00 - 22:00");
-        TimeslotChoiceBox.getItems().add("22:00 - 23:00");
-        TimeslotChoiceBox.getItems().add("23:00 - 00:00");
-    }
-
-    public void initializeBuilding() {
-        BuildingChoiceBox.getItems().add("EWI");
-        BuildingChoiceBox.getItems().add("Drebbelweg");
-    }
-
-    public void initializeCapacity() {
-        CapacityChoiceBox.getItems().add("1 - 10");
-        CapacityChoiceBox.getItems().add("10 - 30");
-        CapacityChoiceBox.getItems().add("30 - 100");
-        CapacityChoiceBox.getItems().add("100 - ");
-    }
-
-    public void initializeRole() {
-        RoleChoiceBox.getItems().add("Teachers only");
-        RoleChoiceBox.getItems().add("Students only");
-    }
-
-    public void initializeFoodAvailable() {
-        FoodAvailableChoiceBox.getItems().add("No Food Available");
-    }
+    private TextField SearchBar;
 
     @FXML
-    private void handleButtonAction(ActionEvent event) throws Exception {
-        Stage stage = null;
-        Parent root = null;
+    private ChoiceBox BikesAvailable;
 
-        if (event.getSource() == ImageToBeAdded) {
-            stage = (Stage) ImageToBeAdded.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("RoomView.fxml"));
-        }
-        if (event.getSource() == CancelBookingButton) {
-            stage = (Stage) CancelBookingButton.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("CancelBookingView.fxml"));
-        }
-        if (event.getSource() == BookingHistoryButton) {
-            stage = (Stage) BookingHistoryButton.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("BookingHistoryView.fxml"));
-        }
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    private TableColumn<Room, String> Image;
+
+    @FXML
+    private TableColumn<Room, String> RoomName;
+
+    @FXML
+    private TableColumn<Room, String> RoomDescription;
+
+    @FXML
+    private TableView<Room> RoomTable;
+
+    @FXML
+    private Button ViewRoomButton;
+
+    public static Room currentSelectedRoom;
+
+     int selectedIndex = 0;
+
+    public SearchViewController() {
     }
+
+
+    @FXML
+    private void initialize() {
+
+        ObservableList<String> TimeslotList = FXCollections.observableArrayList();
+        ObservableList<String> RoleList = FXCollections.observableArrayList();
+        ObservableList<String> CapacityList = FXCollections.observableArrayList();
+        ObservableList<String> BuildingList = FXCollections.observableArrayList();
+        ObservableList<String> BikeList = FXCollections.observableArrayList();
+        ObservableList<String> FoodList = FXCollections.observableArrayList();
+
+
+        TimeslotList.addAll("8:00 - 9:00","10:00 - 11:00","11:00 - 12:00",
+                "12:00 - 13:00","13:00 - 14:00","14:00 - 15:00","15:00 - 16:00",
+                "16:00 - 17:00","17:00 - 18:00","18:00 - 19:00","19:00 - 20:00",
+                "20:00 - 21:00","21:00 - 22:00","23:00 - 24:00");
+        RoleList.addAll("Teachers only");
+        CapacityList.addAll("1-5", "5-10", "5-20", "20-");
+        BuildingList.addAll("Ewi", "Library");
+        BikeList.addAll("1-5","5-10", "10-20","20-");
+        FoodList.addAll("No Food Available", "Food Available");
+
+        //Populating the choicebox
+        TimeslotChoiceBox.setItems(TimeslotList);
+        RoleChoiceBox.setItems(RoleList);
+        CapacityChoiceBox.setItems(CapacityList);
+        BuildingChoiceBox.setItems(BuildingList);
+        BikesAvailable.setItems(BikeList);
+        FoodAvailableChoiceBox.setItems(FoodList);
+
+        try {
+            // Initialize the room table with the three columns.
+            Image.setCellValueFactory(cell -> new SimpleStringProperty(String.valueOf(cell.getValue().getRoomPhoto().get())));
+            RoomName.setCellValueFactory(cell -> cell.getValue().getRoomName());
+            RoomDescription.setCellValueFactory(cell -> new SimpleStringProperty("Capacity: "+(cell.getValue().getRoomBuilding().get())+
+                    "\n"+cell.getValue().getRoomDescription().get()));
+
+
+            // Add observable list data to the table
+            RoomTable.setItems(Room.getRoomData());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public Room getSelectedRoom() {
+        if (RoomTable.getSelectionModel().getSelectedIndex() >= 0) {
+            return RoomTable.getSelectionModel().getSelectedItem();
+        } else {
+            return null;
+        }
+    }
+
+    public int getSelectedIndex() {
+        return RoomTable.getSelectionModel().getSelectedIndex();
+    }
+
+
+    /**
+     * handles button clicks
+     */
+    public void CancelBookingClicked(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        //Cancel Booking view not there yet
+        ///CancelBookingView cb = new AdminManageRoomView();
+        ///cb.start(stage);
+    }
+
+    public void BookingHistoryClicked(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        //Booking history view not there yet
+        //BookingHistoryView bh = new AdminManageRoomView();
+        //bh.start(stage);
+    }
+
+    public void ViewRoomButtonClicked(ActionEvent event) {
+        selectedIndex = getSelectedIndex();
+        try {
+            if (selectedIndex >= 0) {
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                RoomView rv = new RoomView();
+                rv.start(stage);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("No Selection");
+                alert.setContentText("Please select a room from the table");
+                alert.showAndWait();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public int getSelectedIndexNumber(){return selectedIndex;}
+
+
+
 
 
 }
