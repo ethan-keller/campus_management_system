928876a1f40a5fffd51abdda6608ce661f4b318c
Ethan Keller <e.keller@student.tudelft.nl>
2020-03-14 22:19:04 +0100

    merge confilct fix

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Reservations.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Reservations.java
@@ -27,8 +27,7 @@ public class Reservations {
 
     public Reservations() {
     }
-
-<<<<<<< HEAD
+    
     /**
      * Builder
      *
@@ -38,10 +37,7 @@ public class Reservations {
      * @param starting_time String formatted like "". //TODO time format
      * @param ending_time String formatted like "". //TODO time format
      */
-    public Reservations(int id, String username, String date, String starting_time, String ending_time) {
-=======
     public Reservations(int id, String username, int room, String date, String starting_time, String ending_time) {
->>>>>>> origin/testing
         this.id = id;
         this.username = username;
         this.room = room;
