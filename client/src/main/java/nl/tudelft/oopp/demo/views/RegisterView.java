39f81cd58849eaf6b1366e685cb1a74441bd9bb7
Mathijs <m.c.l.vanderschoot@student.tudelft.nl>
2020-03-19 15:27:15 +0100

    Checkstyle

--- client/src/main/java/nl/tudelft/oopp/demo/views/RegisterView.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/RegisterView.java
@@ -1,14 +1,15 @@
 package nl.tudelft.oopp.demo.views;
 
-import java.io.IOException;
-import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class RegisterView extends Application{
+import java.io.IOException;
+import java.net.URL;
+
+public class RegisterView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
