79d8fbaf86a528650b4dd0c63e3fceffae163d63
ovandam <o.m.d.vandam@student.tudelft.nl>
2020-03-23 17:39:49 +0100

    -fixed failing test

--- server/src/test/java/nl/tudelft/oopp/demo/controllers/LoginControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/controllers/LoginControllerTest.java
@@ -30,7 +30,7 @@ class LoginControllerTest {
         assertEquals("teacher", loginCont.getUser("logintest2", "login"));
         userCont.createUser("logintest3", "login", 0);
         assertEquals("admin", loginCont.getUser("logintest3", "login"));
-        assertEquals("notFound", loginCont.getUser("thisDoesNotExist", "pizza"));
+        assertEquals("not_found", loginCont.getUser("thisDoesNotExist", "pizza"));
         assertEquals("wrongPassword", loginCont.getUser("logintest", "wrong"));
         userCont.createUser("logintest4", "login", -2);
         assertEquals("error", loginCont.getUser("logintest4", "login"));
