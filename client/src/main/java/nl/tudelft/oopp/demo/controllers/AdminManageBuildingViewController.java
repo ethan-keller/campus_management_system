7e9930e0f35275cc2b52718d5cc76e188a8254da
lverhaegh <L.Verhaegh@student.tudelft.nl>
2020-03-23 13:58:41 +0100

    A few more errors

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminManageBuildingViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminManageBuildingViewController.java
@@ -46,9 +46,11 @@ public class AdminManageBuildingViewController {
     private void initialize() {
         try {
             // Initialize the room table with the four columns.
-            buildingIdColumn.setCellValueFactory(cell -> new SimpleStringProperty(String.valueOf(cell.getValue().getBuildingId().get())));
+            buildingIdColumn.setCellValueFactory(cell -> new SimpleStringProperty(
+                    String.valueOf(cell.getValue().getBuildingId().get())));
             buildingNameColumn.setCellValueFactory(cell -> cell.getValue().getBuildingName());
-            buildingRoomCountColumn.setCellValueFactory(cell -> new SimpleStringProperty(String.valueOf(cell.getValue().getBuildingRoom_count().get())));
+            buildingRoomCountColumn.setCellValueFactory(cell -> new SimpleStringProperty(
+                    String.valueOf(cell.getValue().getBuildingRoom_count().get())));
             buildingAddressColumn.setCellValueFactory(cell -> cell.getValue().getBuildingAddress());
 
             // Add observable list data to the table
@@ -130,10 +132,13 @@ public class AdminManageBuildingViewController {
             BuildingEditDialogView view = new BuildingEditDialogView();
             view.start(stage);
             Building tempBuilding = BuildingEditDialogController.building;
-            if (tempBuilding == null) return;
+            if (tempBuilding == null) {
+                return;
+            }
 
             // TODO: Check that building creation was succesful before displaying alert
-            BuildingServerCommunication.createBuilding(tempBuilding.getBuildingName().get(), tempBuilding.getBuildingRoom_count().get(), tempBuilding.getBuildingAddress().get());
+            BuildingServerCommunication.createBuilding(tempBuilding.getBuildingName().get(),
+                    tempBuilding.getBuildingRoom_count().get(), tempBuilding.getBuildingAddress().get());
             refresh();
 
             Alert alert = new Alert(AlertType.INFORMATION);
@@ -165,7 +170,9 @@ public class AdminManageBuildingViewController {
                 if (tempBuilding == null) return;
 
                 // TODO: Check that building edit was successful before displaying alert
-                BuildingServerCommunication.updateBuilding(selectedBuilding.getBuildingId().get(), tempBuilding.getBuildingName().get(), tempBuilding.getBuildingRoom_count().get(), tempBuilding.getBuildingAddress().get());
+                BuildingServerCommunication.updateBuilding(selectedBuilding.getBuildingId().get(),
+                        tempBuilding.getBuildingName().get(), tempBuilding.getBuildingRoom_count().get(),
+                        tempBuilding.getBuildingAddress().get());
                 refresh();
 
                 Alert alert = new Alert(AlertType.INFORMATION);
