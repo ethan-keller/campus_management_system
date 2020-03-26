d45bb73281364fda98d9352836b10f26364314b0
lverhaegh <L.Verhaegh@student.tudelft.nl>
2020-03-26 16:09:32 +0100

    actually fixed the last 2 non test serverdside checkstyle errors

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/ItemRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/ItemRepository.java
@@ -1,5 +1,7 @@
 package nl.tudelft.oopp.demo.repositories;
 
+import java.util.List;
+
 import nl.tudelft.oopp.demo.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -7,8 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
