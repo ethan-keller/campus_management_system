07e8bb1b16945abdb84b9b187d202c27d3b918b0
dtgupta <d.t.gupta@student.tudelft.nl>
2020-03-27 19:44:17 +0300

    Hopefully final checkstyle changes

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginViewController.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
+
 import javafx.scene.control.Label;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import nl.tudelft.oopp.demo.communication.LoginServerCommunication;
