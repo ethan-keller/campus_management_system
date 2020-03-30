d7073b808169b57f0c7469b9bae1e340901b71c1
ovandam <o.m.d.vandam@student.tudelft.nl>
2020-03-30 19:27:28 +0200

    -split registerController

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.RegisterServerCommunication;
+import nl.tudelft.oopp.demo.logic.RegisterViewLogic;
 import nl.tudelft.oopp.demo.views.LoginView;
 
 
@@ -47,52 +48,21 @@ public class RegisterViewController {
         String passwordTxt = password.getText();
         String rePasswordTxt = rePassword.getText();
 
-        Pattern upperCasePattern = Pattern.compile("[A-Z]");
-        Pattern characters = Pattern.compile("[!@#$%^&*`~<,>./?:;'{|+=_-]");
-        Pattern space = Pattern.compile(" ");
-
-        //Checks whether the password username field is left empty or not.
-        if (username.getText().trim().isEmpty()) {
-            usernameLabel.setText("This field cannot be left empty !");
+        String usernameResponse = RegisterViewLogic.checkUsername(usernameTxt);
+        String passwordResponse = RegisterViewLogic.checkPassword(passwordTxt);
+        String rePasswordResponse = RegisterViewLogic.checkRePassword(passwordTxt, rePasswordTxt);
+        if(!usernameResponse.equals("Good!")){
+            usernameLabel.setText(usernameResponse);
             usernameLabel.setStyle("-fx-text-fill: red");
-        
-            //Checks whether the password field is left empty.
-        } else if (password.getText().trim().isEmpty()) {
-            passwordLabel.setText("This field cannot be left empty !");
+        } else if (!passwordResponse.equals("Good!")) {
+            passwordLabel.setText(passwordResponse);
             passwordLabel.setStyle("-fx-text-fill: red");
-
-            //Checks whether the password field is atleast 8 characters long.
-        } else if (password.getText().length() < 8) {
-            passwordLabel.setText("The password needs to have atleast 8 characters !");
-            passwordLabel.setStyle("-fx-text-fill: red");
-
-            //Checks whether the password matches the repeat password field
-        } else if (!passwordTxt.equals(rePasswordTxt)) {
+        } else if (!rePasswordResponse.equals("Good!")) {
             rePasswordLabel.setText("The password needs to be the same !");
             rePasswordLabel.setStyle("-fx-text-fill: red");
-
-            //Checks whether the password contains atleast one numeric value
-        } else if (!passwordTxt.matches(".*\\d.*")) {
-            passwordLabel.setText("The password needs to have atleast 1 numeric value !");
-            passwordLabel.setStyle("-fx-text-fill: red");
-        } else if (!upperCasePattern.matcher(passwordTxt).find()) {
-            passwordLabel.setText("The password needs to have atleast 1 Upper Case letter !");
-            passwordLabel.setStyle("-fx-text-fill: red");
-        } else if (space.matcher(usernameTxt).find()) {
-            usernameLabel.setText("The username is not allowed to have any spaces !");
-            usernameLabel.setStyle("-fx-text-fill: red");
-        } else if (space.matcher(passwordTxt).find()) {
-            passwordLabel.setText("The password is not allowed to have any spaces !");
-            passwordLabel.setStyle("-fx-text-fill: red");
-        } else if (characters.matcher(usernameTxt).find()) {
-            usernameLabel.setText("The username is not allowed to have any punctuations !");
-            usernameLabel.setStyle("-fx-text-fill: red");
-        } else if (characters.matcher(passwordTxt).find()) {
-            passwordLabel.setText("The password is not allowed to have any punctuations !");
-            passwordLabel.setStyle("-fx-text-fill: red");
-
+        }
             //Server connection is established.
-        } else {
+         else {
             alert.setContentText(RegisterServerCommunication.sendRegister(usernameTxt, passwordTxt));
             alert.showAndWait();
         }
