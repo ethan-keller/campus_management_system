d09c98100cf6cf02335b2486a85dd26f3ddd66d1
dtgupta <d.t.gupta@student.tudelft.nl>
2020-03-30 14:02:01 +0300

    Final checkstyle fixes

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterViewController.java
@@ -6,12 +6,11 @@ import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ToggleGroup;
 
 import javafx.stage.Stage;
