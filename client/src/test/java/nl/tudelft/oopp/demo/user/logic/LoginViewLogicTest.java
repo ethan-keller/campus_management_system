048a9db62763b125807d8835ecd7d5a2defee94a
ovandam <o.m.d.vandam@student.tudelft.nl>
2020-04-03 12:38:50 +0200

    -problems in loginViewLogicTest

--- client/src/test/java/nl/tudelft/oopp/demo/user/logic/LoginViewLogicTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/user/logic/LoginViewLogicTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.demo.user.logic;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -8,9 +9,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginViewLogicTest {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private Label passwordLabel;
 
     @Test
-    public void isValidInputTest(){
-
+    void isValidInputTest(){
+        username = new TextField();
+        username.setText("");
     }
 }
