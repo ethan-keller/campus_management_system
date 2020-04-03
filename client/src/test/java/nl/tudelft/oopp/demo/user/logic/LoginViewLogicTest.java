9c8b6b0c3fbddb82dd6d588ea2cac989abe0cd17
ovandam <o.m.d.vandam@student.tudelft.nl>
2020-04-03 13:35:31 +0200

    -refactor some of loginViewController and LoginViewLogic -Made a test for LoginViewLogic

--- client/src/test/java/nl/tudelft/oopp/demo/user/logic/LoginViewLogicTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/user/logic/LoginViewLogicTest.java
@@ -1,29 +1,24 @@
 package nl.tudelft.oopp.demo.user.logic;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginViewLogicTest {
-    @FXML
-    private TextField username;
-
-    @FXML
-    private PasswordField password;
-
-    @FXML
-    private Label usernameLabel;
-
-    @FXML
-    private Label passwordLabel;
 
     @Test
     void isValidInputTest(){
-        username = new TextField();
-        username.setText("");
+        String usernameInput = "";
+        String passwordInput = "";
+
+        assertEquals("The username field cannot be left empty !", LoginViewLogic.isValidInput(usernameInput, passwordInput));
+
+        usernameInput = "username";
+
+        assertEquals("The password field cannot be left empty !", LoginViewLogic.isValidInput(usernameInput, passwordInput));
+
+        passwordInput = "Password01";
+
+        assertEquals("Good!", LoginViewLogic.isValidInput(usernameInput, passwordInput));
     }
 }
