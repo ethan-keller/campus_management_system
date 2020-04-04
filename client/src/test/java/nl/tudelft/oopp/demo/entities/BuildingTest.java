7cc70c6aace9a2b6afddbc6af26267d5b4fa8ac3
Mathijs <m.c.l.vanderschoot@student.tudelft.nl>
2020-04-04 12:41:51 +0200

    commit

---
+++ client/src/test/java/nl/tudelft/oopp/demo/entities/BuildingTest.java
@@ -0,0 +1,87 @@
+package nl.tudelft.oopp.demo.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildingTest {
+
+    private Building building;
+
+    @BeforeEach
+    void setUp() {
+        building = new Building(30, "testBuilding");
+    }
+
+    @Test
+    void getBuildingId() {
+
+    }
+
+    @Test
+    void setBuildingId() {
+    }
+
+    @Test
+    void getBuildingName() {
+    }
+
+    @Test
+    void setBuildingName() {
+    }
+
+    @Test
+    void getBuildingRoomCount() {
+    }
+
+    @Test
+    void setBuildingRoomCount() {
+    }
+
+    @Test
+    void getBuildingAddress() {
+    }
+
+    @Test
+    void setBuildingAddress() {
+    }
+
+    @Test
+    void getBuildingMaxBikes() {
+    }
+
+    @Test
+    void setBuildingMaxBikes() {
+    }
+
+    @Test
+    void getOpeningTime() {
+    }
+
+    @Test
+    void setOpeningTime() {
+    }
+
+    @Test
+    void getClosingTime() {
+    }
+
+    @Test
+    void setClosingTime() {
+    }
+
+
+
+    @Test
+    void getBuildingData() {
+    }
+
+    @Test
+    void getBuildingById() {
+    }
+
+    @Test
+    void getBuildingByFoodId() {
+    }
+}
