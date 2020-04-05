8e138be8429ed658bf3fb43bb1432b03d6887f7f
Hao Wang <H.Wang-26@student.tudelft.nl>
2020-04-05 20:56:33 +0200

    Checkstyle fixed

--- client/src/main/java/nl/tudelft/oopp/demo/admin/logic/AdminLogic.java
+++ client/src/main/java/nl/tudelft/oopp/demo/admin/logic/AdminLogic.java
@@ -1,5 +1,7 @@
 package nl.tudelft.oopp.demo.admin.logic;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.control.TableView;
 
 import nl.tudelft.oopp.demo.admin.controller.AdminManageFoodViewController;
@@ -17,8 +19,7 @@ import nl.tudelft.oopp.demo.entities.Reservation;
 import nl.tudelft.oopp.demo.entities.Room;
 import nl.tudelft.oopp.demo.entities.User;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class AdminLogic {
 
